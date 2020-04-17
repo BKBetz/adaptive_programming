2d250a347e2bbe09526b9335af2ca50af43a5bb6
BKBetz <brandonbetz@live.nl>
2020-04-17 22:11:23 +0200

    made calc function for gameshop

--- formatieve_opdracht_1a/gameshop/src/Game.java
+++ formatieve_opdracht_1a/gameshop/src/Game.java
@@ -1,2 +1,26 @@
+import java.time.LocalDate;
+
 public class Game {
+    private String name;
+    private int release;
+    private double price;
+
+    public Game(String nm, int rl, double pr) {
+        this.name = nm;
+        this.release = rl;
+        this.price = pr;
+    }
+
+    public double calcPrice() {
+        int date = LocalDate.now().getYear();
+        int game_age = date - this.release;
+        double discount = 0.3;
+        double new_price = this.price;
+
+        for(int i = 0; i < game_age; i++) {
+            new_price = new_price * (1 - discount);
+        }
+
+        return this.price = new_price;
+    }
 }
--- formatieve_opdracht_1a/gameshop/src/Persoon.java
+++ formatieve_opdracht_1a/gameshop/src/Persoon.java
@@ -1,2 +1,9 @@
 public class Persoon {
+    private String name;
+    private int budget;
+
+    public Persoon(String nm, int bd) {
+        this.name = nm;
+        this.budget = bd;
+    }
 }
