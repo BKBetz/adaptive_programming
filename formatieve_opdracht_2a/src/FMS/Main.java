ae4d0d548811e5c2c3341d9342ad6506ecef7c9a
BKBetz <brandonbetz@live.nl>
2020-05-06 15:25:43 +0200

    finished a2

--- formatieve_opdracht_2a/src/FMS/Main.java
+++ formatieve_opdracht_2a/src/FMS/Main.java
@@ -1,5 +1,6 @@
 package FMS;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class Main {
         Node s2 = new Node("s2");
         Node s3 = new Node("s3");
 
+        //set connections
+        s0.setConnections("s1", "s2");
+        s1.setConnections("s1", "s2");
+        s2.setConnections("none", "s3");
+        s3.setConnections("s3", "s0");
+
         // get user input
         String code = "";
         System.out.println("Enter a string with only A's and B's");
@@ -16,7 +23,33 @@ public class Main {
         String user_input = input.next();
 
         if(user_input.matches("^[a-bA-B]+$")) {
+            // machine
+            ArrayList<String> path = new ArrayList<String>();
+            String path_step = "s0";
+            path.add(path_step);
             code = user_input;
+            for(String s : code.split("")) {
+                switch (path_step) {
+                    case "s0":
+                        path_step = s0.machinePath(s.toUpperCase());
+                        path.add(path_step);
+                        break;
+                    case "s1":
+                        path_step = s1.machinePath(s.toUpperCase());
+                        path.add(path_step);
+                        break;
+                    case "s2":
+                        path_step = s2.machinePath(s.toUpperCase());
+                        path.add(path_step);
+                        break;
+                    case "s3":
+                        path_step = s3.machinePath(s.toUpperCase());
+                        path.add(path_step);
+                        break;
+                }
+            }
+
+            System.out.println(path);
         }
         else {
             System.out.println("String contains more than just A & B");
--- formatieve_opdracht_2a/src/FMS/Node.java
+++ formatieve_opdracht_2a/src/FMS/Node.java
@@ -3,13 +3,35 @@ import java.util.HashMap;
 
 public class Node {
     private String name;
+    private HashMap<String, String> connections = new HashMap<String, String>();
 
     public Node(String nm) {
         this.name = nm;
     }
 
     public void setConnections(String con_1, String con_2) {
-        HashMap<String, String> connections = new HashMap<String, String>();
-        connections.put(con_1, con_2);
+        this.connections.put("A", con_1);
+        this.connections.put("B", con_2);
+    }
+
+    public String machinePath(String code) {
+        if(code.equals("A")) {
+            return connections.get("A");
+        }
+
+        else if(code.equals("B")) {
+            return connections.get("B");
+        }
+        else {
+            return "none";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "name='" + name + '\'' +
+                ", connections=" + connections +
+                '}';
     }
 }
