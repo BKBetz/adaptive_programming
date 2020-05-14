5fd25eee9ba6868184507d9c4358c893cc1af2ad
BKBetz <brandonbetz@live.nl>
2020-05-15 01:22:51 +0200

    Implemented 2a and made machine recursive

--- opdracht_2/src/FMS/Node.java
+++ opdracht_2/src/FMS/Node.java
@@ -1,38 +1,39 @@
 package FMS;
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public class Node {
     private String name;
-    private HashMap<String, Object> connections = new HashMap<String, Object>();
-    private String type;
+    private HashMap<String, Node> connections = new HashMap<String, Node>();
 
-    public Node(String nm, String tp) {
+
+    public Node(String nm) {
         this.name = nm;
-        this.type = tp;
     }
 
-    public void setConnections(String[] links) {
+    public void setConnections(Node[] links) {
         for(int i = 1; i < links.length + 1; i++) {
             connections.put(Integer.toString(i), links[i -1]);
         }
     }
 
-    public Object machinePath(String code) {
-        if(Integer.parseInt(code) < connections.size()) {
-            return connections.get(code);
+    public String getName() {
+        return this.name;
+    }
+
+    public Node machinePath(String code) {
+        if(code == "") {
+            Node end = new Node("stop");
+            return end;
         }
 
         else {
-            return "none";
+            System.out.println(code);
+            if (Integer.parseInt(code) <= connections.size()) {
+                return connections.get(code);
+            } else {
+                Node end = new Node("stop");
+                return end;
+            }
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "name='" + name + '\'' +
-                ", connections=" + connections +
-                '}';
     }
 }
